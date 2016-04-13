c30b4c97cfe077d66457e81c72ac3122f6d16725
Pablo Pernias <pablo@pernias.com>
2016-04-13 18:51:04 +0200

    added movies from actor query

--- app.js
+++ app.js
@@ -44,6 +44,10 @@ var app = angular.module("app", [])
     return "https://query.wikidata.org/bigdata/namespace/wdq/sparql?query=%0ASELECT%20DISTINCT%20%3Fs%20%3Flabel%20%3Fdescription%20%3Fthumbnail%20WHERE%20%7B%0A%20%7B%3Fs%20wdt%3AP31%20wd%3AQ11424%20%7D%20UNION%20%7B%3Fs%20wdt%3AP106%20wd%3AQ10800557%7D%20UNION%20%7B%3Fs%20wdt%3AP106%20wd%3AQ2526255%7D.%0A%20%3Fs%20rdfs%3Alabel%20%3Flabel%20.%0A%20OPTIONAL%20%7B%0A%20%20%20%3Fs%20wdt%3AP18%20%3Fthumbnail%20.%0A%20%20%20%3Fs%20schema%3Adescription%20%3Fdescription%20.%0A%20%20%20FILTER%20(lang(%3Fdescription)%20%3D%20%27en%27)%0A%20%7D%0A%20FILTER%20(lang(%3Flabel)%20%3D%20%27en%27)%0A%20FILTER(contains(lcase(%3Flabel)%2C%20lcase(%27"+query+"%27)))%20.%0A%7D%0ALIMIT%20100%0A";
   }
 
+  var moviesFromActor = function(id){
+    return "https://query.wikidata.org/bigdata/namespace/wdq/sparql?query=PREFIX%20wd%3A%20%3Chttp%3A%2F%2Fwww.wikidata.org%2Fentity%2F%3E%0APREFIX%20wdt%3A%20%3Chttp%3A%2F%2Fwww.wikidata.org%2Fprop%2Fdirect%2F%3E%0APREFIX%20wikibase%3A%20%3Chttp%3A%2F%2Fwikiba.se%2Fontology%23%3E%0A%0ASELECT%20DISTINCT%20%3Fs%20%3Flabel%20WHERE%20%7B%0A%20%3Fs%20wdt%3AP31%20wd%3AQ11424%20.%0A%20%3Fs%20rdfs%3Alabel%20%3Flabel%20.%0A%20%3Fs%20wdt%3AP161%20wd%3A"+id+"%0A%20FILTER%20(lang(%3Flabel)%20%3D%20%27en%27)%0A%20%23FILTER(contains(lcase(%3Flabel)%2C%20lcase(%27star%20wars%27)))%20.%0A%7D%0ALIMIT%20100%0A";
+  }
+
   var search = function(query){
 
     if(query.indexOf('#') != -1) query = query.substring(0, query.indexOf('#'));
